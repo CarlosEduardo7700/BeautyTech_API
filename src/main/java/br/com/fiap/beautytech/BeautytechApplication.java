d1c202f4ea2c4e7ef014bd0f8b675b57d41ea886
CarlosEduardo7700 <rm552164@fiap.com.br>
2024-04-10 12:10:21 -0300

    Enviando projeto para a plataforma do GitHub

---
+++ src/main/java/br/com/fiap/beautytech/BeautytechApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.beautytech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautytechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautytechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/beautytech/BeautytechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.beautytech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautytechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
