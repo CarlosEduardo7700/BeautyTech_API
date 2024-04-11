8e8d5995f11d97127f65160981d549595ddf3bea
C4MJR <junior.carlinhos05@gmail.com>
2024-04-11 15:05:15 -0300

    Usando anotações de validação para cadastro e atualização dos dados de clientes.

--- src/main/java/br/com/fiap/beautytech/dtos/AtualizarClienteDto.java
+++ src/main/java/br/com/fiap/beautytech/dtos/AtualizarClienteDto.java
@@ -2,13 +2,21 @@ package br.com.fiap.beautytech.dtos;
 
 import br.com.fiap.beautytech.models.enums.EstadoCivil;
 import br.com.fiap.beautytech.models.enums.Genero;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
 public record AtualizarClienteDto(
+        @Size(max = 11, min = 11, message = "CPF tem que ter 11 números.")
         String cpf,
+        @Size(max = 150)
         String nome,
+        @Email(message = "Formato de email inválido!")
+        @Size(max = 100)
         String email,
+        @Past(message = "Data de nascimento precisa ser no passado.")
         LocalDate dataDeNascimento,
         EstadoCivil estadoCivil,
         Genero genero,
