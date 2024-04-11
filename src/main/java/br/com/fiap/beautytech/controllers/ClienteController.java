e277ff9f162c52ec16806d3bd43a937713db7424
kauezin05 <kauezinho_naruto@hotmail.com>
2024-04-11 12:05:08 -0300

    Criando a requisição GET para o ClienteController.

--- src/main/java/br/com/fiap/beautytech/controllers/ClienteController.java
+++ src/main/java/br/com/fiap/beautytech/controllers/ClienteController.java
@@ -2,18 +2,18 @@ package br.com.fiap.beautytech.controllers;
 
 import br.com.fiap.beautytech.dtos.CadastroClienteDto;
 import br.com.fiap.beautytech.dtos.DetalhesClienteDto;
+import br.com.fiap.beautytech.dtos.ListagemClienteDto;
 import br.com.fiap.beautytech.models.Cliente;
 import br.com.fiap.beautytech.repositories.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("clientes")
@@ -31,5 +31,13 @@ public class ClienteController {
         URI uri = uriBuilder.path("/clientes/{id}").buildAndExpand(cliente.getId()).toUri();
 
         return ResponseEntity.created(uri).body(new DetalhesClienteDto(cliente));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ListagemClienteDto>> buscarTudo(Pageable pageable) {
+        var listaDto = repository.findAll(pageable)
+                .stream().map(ListagemClienteDto::new).toList();
+        return ResponseEntity.ok(listaDto);
+
     }
 }
