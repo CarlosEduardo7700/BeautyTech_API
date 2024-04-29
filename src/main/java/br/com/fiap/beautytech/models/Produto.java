8241da0832b35b914016211c5ec8b67846fecd05
Reginaldo0206 <vitormiranda4321@outlook.com>
2024-04-29 16:33:10 -0300

    Criação e mapeamento da entidade Produto.

---
+++ src/main/java/br/com/fiap/beautytech/models/Produto.java
@@ -0,0 +1,45 @@
+package br.com.fiap.beautytech.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "BT_PRODUTO")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Produto {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ID_PRODUTO")
+    private Long id;
+
+    @Column(name = "NM_PRODUTO", length = 150, nullable = false)
+    private String nome;
+
+    @Column(name = "DESC_PRODUTO", length = 150, nullable = false)
+    private String descricao;
+
+    @Column(name = "VL_PRODUTO", precision = 8, scale = 2, nullable = false)
+    private BigDecimal preco;
+
+    @Column(name = "DT_FAB_PRODUTO", nullable = false)
+    private LocalDate dataDeFabricacao;
+
+    @Column(name = "DT_VALIDADE", nullable = false)
+    private LocalDate validade;
+
+    @Column(name = "DT_CADASTRO", nullable = false)
+    private LocalDate dataDeCadastro;
+
+    @Column(name = "IMG_PRODUTO", length = 300, nullable = false)
+    private String caminhoDaimagem;
+}
