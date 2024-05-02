98feb065b3705997a807159e621a3b9ea3346639
CarlosEduardo7700 <rm552164@fiap.com.br>
2024-05-01 23:01:21 -0300

    Criação e mapeamento da entidade CategoriaDeProduto.

---
+++ src/main/java/br/com/fiap/beautytech/models/CategoriaDeProduto.java
@@ -0,0 +1,25 @@
+package br.com.fiap.beautytech.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "BT_CATEGORIA")
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+public class CategoriaDeProduto {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ID_CATEGORIA")
+    private Long id;
+
+    @Column(name = "NM_CATEGORIA", length = 50, nullable = false)
+    private String nome;
+
+    @Column(name = "DESC_CATEGORIA", length = 200, nullable = false)
+    private String descricao;
+}
