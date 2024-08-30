ed503c5ecdbd620d2cdcb3c07e5a7967a96ba143
CarlosEduardo7700 <rm552164@fiap.com.br>
2024-08-29 21:40:26 -0300

    Configurando o SWAGGER para receber o token JWT

--- src/main/java/br/com/fiap/beautytech/configurations/OpenApiConfig.java
+++ src/main/java/br/com/fiap/beautytech/configurations/OpenApiConfig.java
@@ -1,8 +1,12 @@
 package br.com.fiap.beautytech.configurations;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
@@ -16,7 +20,17 @@ import io.swagger.v3.oas.annotations.servers.Server;
         ),
         servers = {
                 @Server(description = "Dev Env", url = "http://localhost:8080")
-        }
+        },
+        security = @SecurityRequirement(name = "bearedJWT")
+)
+
+@SecurityScheme(
+        name="bearedJWT",
+        description = "Autenticação básica JWT",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
 )
 
 public class OpenApiConfig {
